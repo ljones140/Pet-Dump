83b50929cf71b97fb12336d6d820ec4974552b4b
lewis <ljones140@gmail.com>
2015-10-16 12:17:50 +0100

    first basic test passing

--- spec/features/pet_feature_spec.rb
+++ spec/features/pet_feature_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 feature 'pets' do
-  context 'when user not signed in ' do
+  
+  let(:user){ create(:user) }
+
+  context 'when user not signed in' do
     scenario 'displays sign up message' do
       visit root_path
-      expect(page).to have_content 'Join us to track your pets business'
+      expect(page).to have_content('Join us to track your pets business')
+    end
+  end
+
+  context 'when user signed in' do
+    scenario 'displays no pets added' do
+      visit root_path
+      sign_in_as(user)
+      expect(page).not_to have_content('Join us to track your pets business')
+      expect(page).to have_content('no pets added yet')
+      expect(page).to have_link('add a pet')
     end
   end
  end
