f756ded2c42c327ed6b1c8b7b9d45174971ef1f8
lewis <ljones140@gmail.com>
2015-10-16 20:05:27 +0100

    remove animal from pet model not need at present design state

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016115901) do
+ActiveRecord::Schema.define(version: 20151016190213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pets", force: :cascade do |t|
     t.string   "name"
-    t.string   "animal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
--- spec/factories/pets.rb
+++ spec/factories/pets.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :pet do
     name 'Trigger'
-    animal 'dog'
   end
 end
