2a3ff94aa1cc850da82cf910df780641d23bf5a7
lewis <ljones140@gmail.com>
2015-10-16 20:29:03 +0100

    more refactoring of tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     password_confirmation 'testtest'
 
     factory :user_with_pet do
-      after(:create) {|instance| create_list(:pet, 1, user: instance) }
+      after(:create) {|instance| create(:pet, user: instance) }
     end
   end
 end
