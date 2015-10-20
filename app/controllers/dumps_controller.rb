ef1cfeb23b4614d8732a5e69aacc0df5fbc0f7a1
lewis <ljones140@gmail.com>
2015-10-20 23:11:12 +0100

    dump time is recorded correctly and shown

--- app/controllers/dumps_controller.rb
+++ app/controllers/dumps_controller.rb
@@ -1,8 +1,8 @@
 class DumpsController < ApplicationController
 
   def create
-    @pet = Pet.find(params[:pet_id])
-    @pet.dumps.create({ time_went: Time.now })
+    pet = Pet.find(params[:pet_id])
+    pet.dumps.create({ time_went: Time.now.localtime  })
     redirect_to pets_path
   end
 
--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -1,2 +1,7 @@
 module PetsHelper
+
+  def last_dump(pet)
+    pet.dumps.maximum("time_went").localtime.strftime("%H:%M %d/%m/%Y")
+  end
+
 end
