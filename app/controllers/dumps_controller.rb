52a548b4b7d835e58145ca6fa9edff7179055fe2
lewis <ljones140@gmail.com>
2015-10-20 23:14:30 +0100

    refactor before pull request

--- app/controllers/dumps_controller.rb
+++ app/controllers/dumps_controller.rb
@@ -2,7 +2,7 @@ class DumpsController < ApplicationController
 
   def create
     pet = Pet.find(params[:pet_id])
-    pet.dumps.create({ time_went: Time.now.localtime  })
+    pet.dumps.create({ time_went: Time.now })
     redirect_to pets_path
   end
 
