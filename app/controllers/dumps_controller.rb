992fc99a727d09f0544eee4087986218343efc43
lewis <ljones140@gmail.com>
2015-10-28 18:40:09 +0000

    time can be specified

--- app/controllers/dumps_controller.rb
+++ app/controllers/dumps_controller.rb
@@ -2,9 +2,19 @@ class DumpsController < ApplicationController
 
   def create
     pet = Pet.find(params[:pet_id])
-    pet.dumps.create({ time_went: Time.now })
+    dump_time = Time.new(*flatten_to_date(params[:dump]))
+    pet.dumps.create({ time_went: dump_time  })
     redirect_to pets_path
   end
 
+  private
+
+  def dump_params
+    params.permit(:dump)
+  end
+
+  def flatten_to_date(dump)
+    %w(1 2 3 4 5).map { |e| dump["time_went(#{e}i)"].to_i }
+  end
 end
 
