af268e6ac8a685b357bb28c46177d05d9c614809
lewis <ljones140@gmail.com>
2015-11-12 10:08:03 +0900

    new feature branch for probability

--- spec/factories/pets.rb
+++ spec/factories/pets.rb
@@ -5,5 +5,9 @@ FactoryGirl.define do
     trait :with_dump do
      after(:create) { |instance| create(:dump, pet: instance) }
     end 
+
+    trait :with_daily_dumps do
+     after(:create) { |instance| create(:dump, pet: instance) }
+    end 
   end
 end
