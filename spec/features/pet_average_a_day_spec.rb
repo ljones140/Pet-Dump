a9eac0ca6d8b533728601050b7f5d7d94f67a5ed
lewis <ljones140@gmail.com>
2015-11-23 12:18:51 +0000

    test for no dumps no average passing

--- spec/features/pet_average_a_day_spec.rb
+++ spec/features/pet_average_a_day_spec.rb
@@ -4,8 +4,15 @@ feature 'average dumps a day' do
 
   let(:user_one_daily){ create(:user, :with_one_daily_dump_pet) }
   let(:user_two_daily){ create(:user, :with_two_daily_dump_pet) }
+  let(:user_no_dumps){ create(:user, :with_pet) }
+
+  time = Time.utc(2015, 10, 18, 10, 00)
 
   context 'when pet has daily dumps' do
+
+    before do
+      Timecop.freeze(time)
+    end
 
     scenario 'shows average 1 dump a day' do
       sign_in_as(user_one_daily)
@@ -17,6 +24,16 @@ feature 'average dumps a day' do
       sign_in_as(user_two_daily)
       visit pets_path
       expect(page).to have_content('average dumps a day: 2')
+    end
+
+  end
+
+  context 'when pet has zero dumps' do
+
+    scenario 'shows average 0 dumps a day' do
+      sign_in_as(user_no_dumps)
+      visit pets_path
+      expect(page).not_to have_content('average dumps a day:')
     end
 
   end
