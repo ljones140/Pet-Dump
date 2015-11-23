892f7534dd2597a20bd1f87edf22c60c474b7efc
lewis <ljones140@gmail.com>
2015-11-23 17:13:21 +0000

    probabilty work started

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -9,10 +9,31 @@ class Pet < ActiveRecord::Base
     dumps.values.inject(:+) / dumps.count
   end
 
-  private
+  def need_to_go
+    return "high" if dumps_today < average_dumps_per_day && Time.now.strftime("%H:%M") > avg_dump_times
+    return "low" if dumps_today >= average_dumps_per_day
+    return "unknown"
+  end
+
+  def avg_dump_times
+    size = dump_times.size
+    avg_minutes = dump_times.map do |x|
+      hour, minute = x.split(':')
+      total_minutes = hour.to_i * 60 + minute.to_i
+    end.inject(:+)/size
+    "%s:%s" % avg_minutes.divmod(60).map(&:to_i)
+  end
 
   def daily_dumps
     dumps.group("DATE_TRUNC('day', time_went)").count
+  end
+
+  def dump_times
+    dumps.map(&:time_went).map{ |t| t.strftime "%H:%M" }
+  end
+
+  def dumps_today
+    dumps.where(["time_went >= ?", Time.now.beginning_of_day]).count
   end
 
 end
--- spec/features/pet_need_to_go_spec.rb
+++ spec/features/pet_need_to_go_spec.rb
@@ -4,15 +4,41 @@ feature 'pet dump need to go' do
 
   let(:user){ create(:user, :with_pet) } 
   let(:pet){ user.pets.first }
+  let(:user_one_daily){ create(:user, :with_one_daily_dump_pet) }
 
-  context 'when user signed in with pet' do
+  time = Time.utc(2015, 10, 18, 10, 00)
 
-    scenario 'if pet has no dumps need to go is unknown' do
+  context 'pet with no dumps' do
+
+    scenario 'need to go is unknown' do
       sign_in_as(user)
       visit pets_path
       expect(page).to have_content(pet.name)
       expect(page).not_to have_content('last dump')
-      expect(page).to have_content('need to go: unknown') 
+      expect(page).not_to have_content('need to go:')
+    end
+
+  end
+
+  context 'pet with dumps' do
+
+    before do
+      Timecop.freeze(time)
+    end
+
+    scenario 'should be high if not dumped after average time' do
+      Timecop.travel(Time.utc(2015, 10, 19, 11, 00))
+      sign_in_as(user_one_daily)
+      visit pets_path
+      expect(page).to have_content('need to go: high')
+    end
+
+    scenario 'should be low if dumped before average time' do
+      Timecop.travel(Time.utc(2015, 10, 19, 10, 00))
+      sign_in_as(user_one_daily)
+      visit pets_path
+      click_button('Record Dump')
+      expect(page).to have_content('need to go: low')
     end
 
   end
